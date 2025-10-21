32d5d431718228fc9116c12f1470298c7d87d768
LeRenardeauCode <p.ewan@hotmail.fr>
2025-10-21 09:45:54 +0200

    ajout du .env et connexion bdd sur autre pc

--- config/bdd.js
+++ config/bdd.js
@@ -1,19 +1,20 @@
+import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 // creation de la connexion à la base de données
-const connexion = mysql.createPool({
+const connexion = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
 // test de la connexion
-connexion.getConnection()
-// si la connexion est réussie
-    .then(() =>
-    console.log("database OK 🟢"))
-    // si la connexion échoue
-    .catch(error => console.error("database KO 🔴​", error));
+try {
+  await connexion.ping();
+  console.log("database OK 🟢");
+} catch (error) {
+  console.error("database KO 🔴", error);
+}
 
-export default connexion
+export default connexion;
