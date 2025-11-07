6686c00e0a2b8a48e64ca8c8df02ebb6cac704ed
= <=>
2025-11-07 16:35:59 +0100

    Ajout de la BDD Sql et des containers + le compose de Docker

--- backend/config/bdd.js
+++ backend/config/bdd.js
@@ -3,10 +3,11 @@ import mysql from 'mysql2/promise';
 
 // creation de la connexion à la base de données
 const connexion = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'rootpassword',
+  database: process.env.DB_NAME || 'yugioh_db',
+  port: process.env.DB_PORT || 3306
 });
 
 // test de la connexion
